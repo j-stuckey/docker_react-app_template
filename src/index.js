77ea6f82af57842faa63f45531c1426743831a5e
James Stuckey <jstuckey@rutgers.edu>
2019-05-14 23:39:58 -0400

    Added redux store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 
+import {store} from './redux/store';
+
 import 'normalize.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 registerServiceWorker();
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,27 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import createHistory from 'history/createBrowserHistory';
 
+const initialState = {};
+const enhancers = [];
+const middleware = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+    const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+    if (typeof devToolsExtension === 'function') {
+        enhancers.push(devToolsExtension());
+    }
+}
+
+const composedEnhancers = compose(
+    applyMiddleware(...middleware),
+    ...enhancers
+);
+
+export const store = createStore(
+    combineReducers({
+		
+    }),
+    initialState,
+    composedEnhancers
+);
