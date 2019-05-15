e7e599325a4953005bfbb6d312fdd2ae26407c2c
James Stuckey <jstuckey@rutgers.edu>
2019-05-14 23:46:20 -0400

    Added React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './styles/App.module.css'; // CSS Modules
 
 // Redux-actions might be useful as well. Check it out:
@@ -9,7 +9,9 @@ class App extends Component {
     render() {
         return (
             <div className={styles.AppContainer}>
-                <section className={styles.LeftSection} />
+                <section className={styles.LeftSection}>
+                    <Link to="/about">About</Link>
+                </section>
                 <section className={styles.RightSection}>
                     <p>Hello, world!</p>
                     <p>
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import {store} from './redux/store';
+import { store } from './redux/store';
 
 import 'normalize.css';
 import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
