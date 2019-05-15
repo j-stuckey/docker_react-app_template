72fc789986eb8701c1704dd032d8aa62ef67e8f8
James Stuckey <jstuckey@rutgers.edu>
2019-05-14 23:25:49 -0400

    Added eslintrc

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 
-import styles from './styles/App.module.css';
+import styles from './styles/App.module.css'; // CSS Modules
+
+// Redux-actions might be useful as well. Check it out:
+// https://github.com/redux-utilities/redux-actions/blob/master/docs/introduction/Tutorial.md
 
 class App extends Component {
-
-	render() {
-
+    render() {
         return (
             <div className={styles.AppContainer}>
-				<section className={styles.LeftSection}>
-				</section>
-				<section className={styles.RightSection}>
-					<p>Hello, world!</p>
-				</section>
+                <section className={styles.LeftSection} />
+                <section className={styles.RightSection}>
+                    <p>Hello, world!</p>
+                    <p>
+                        This starter kit should come equipped with Redux,
+                        Redux-Thunk, React Router and CSS Modules. 
+                    </p>
+                </section>
             </div>
         );
     }
